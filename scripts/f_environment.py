a21c5343a5a4f0840fd5adfeaab32c8487379cd5
Vladislav Tumko <56307628+vectorvp@users.noreply.github.com>
2025-06-20 09:03:59 +0400

    refactor: return custom exp name if is set
    For now experiment name returned in automatic way, no matter if is set in config or not.

--- scripts/f_environment.py
+++ scripts/f_environment.py
@@ -156,7 +156,7 @@ def validate_config(config):
     
     experiment_name += f"_sd{config.SEED}"
 
-    return experiment_name
+    return experiment_name if not config.EXPERIMENT_NAME else config.EXPERIMENT_NAME
 
 def find_seed_in_weight(weight_name):
     match = re.search(r'sd(\d+)', weight_name)
